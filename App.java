bc5c9c319002a3d6568f1b80f267d67ce6005013
IsaMoraess <135661414+IsaMoraess@users.noreply.github.com>
2024-04-12 15:30:32 -0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+            String nome;
+            int idade;
+            nome = "Maria";
+            idade = 18;
+            System.out.println(nome+ " tem " +idade+ " anos ");
+            nome = "jose";
+            idade = idade+2;
+            System.out.println(nome+ " tem "+idade+ " anos ");
+    }
+}
